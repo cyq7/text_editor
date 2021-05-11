335dbc20f379449c4936b4d58a572c6e42b4541c
Cezary Kocinski <ckocinski7@wp.pl>
2021-05-11 21:54:03 +0200

    add download functionality

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-	function format(command, value) {
-		document.execCommand(command, false, value);
-	}
+const format = (command, value) => {
+	document.execCommand(command, false, value);
+}
+
+const download = (filename, text) => {
+	//defining the data of the file using encodeURIComponent
+	const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(text));
+
+	//creating hidden element
+	const element = document.createElement('a');
+	element.style.display = 'none';
+
+	element.setAttribute('href', dataStr);
+
+	//adding the download attribute of the the hidden link
+	element.setAttribute('download', filename + ".json");
+	document.body.appendChild(element);
+
+	//link click simulation
+	element.click();
+
+	document.body.removeChild(element);
+}
+
+document.getElementById("download-btn").addEventListener("click", () => {
+
+    const text = document.getElementById("editor").innerText;
+    const filename = document.getElementById("file-name").value;
+
+    download(filename, text);
+}, false);
